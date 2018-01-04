f079d814a0cebb3944ccce63fe4b84b820fae97f
sinchang <sinchangwen@gmail.com>
2018-01-04 20:36:21 +0800

    fix: update regex

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cheerio = require('cheerio')
 const { token, githubId } = require('./config')
 
 const BASE_URL = 'https://api.github.com'
-const regex = /<a\starget='_blank'\srel='nofollow'\shref='https:\/\/app.codesponsor.io\/link(.*)\n(.*)\n<\/a>/g
+const regex = /<a\s(.*)https:\/\/app.codesponsor.io\/link(.*)<\/a>/g
 const accessToken = `token ${token}`
 
 function forkRepo(owner, githubId, repo) {
